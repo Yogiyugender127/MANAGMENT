f09dc10b6270d438d3e408e78c692783d0fd2b8a
Yogiyugender127 <108338489+Yogiyugender127@users.noreply.github.com>
2022-10-17 15:30:23 +0530

    vaasivaadi

--- MANAGMENT/Controllers/HomeController.cs
+++ MANAGMENT/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace MANAGMENT.Controllers
     {
         public ActionResult Index()
         {
+
+            // vaadi vaadi thassa dhiyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
             return View();
         }
 
