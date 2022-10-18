5fc1a41a5d464d83c9497778dc403af5cf163470
Yogiyugender127 <108338489+Yogiyugender127@users.noreply.github.com>
2022-10-18 16:05:49 +0530

    gggggggggggggggggggggggggg

--- MANAGMENT/Controllers/HomeController.cs
+++ MANAGMENT/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MANAGMENT.Controllers
     {
         public ActionResult Index()
         { 
-            //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
             return View();
         }
 
